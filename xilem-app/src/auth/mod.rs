ead51972269a48b1673bce15554f2358770b8c97
ember arlynx <ember@lunar.town>
2025-08-10 23:10:48 -0400

    xilem-app restart from newer xilem version

--- xilem-app/src/auth/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod apple_signin;
-#[cfg(target_os = "ios")]
-pub mod ios;
-
-pub use apple_signin::*;
-#[cfg(target_os = "ios")]
-pub use ios::*;
--- xilem-app/src/main.rs
+++
@@ -1,7 +0,0 @@
-// Copyright 2024 Graph Learning System Authors
-// SPDX-License-Identifier: Apache-2.0
-
-fn main() {
-    // Run the app (demo / showcased entrypoint)
-    abcdeez_app::run();
-}
